49e2882e684ef71df9471ed1022e9bd3c0265905
Seungwon <seungwon604@gmail.com>
2021-11-06 17:14:11 +0900

    1. Worked_by_Seungwon 브랜치 생성   2. 앞으로 필요한 추가 이미지 파일 추가, 폴더로 정리   3. 외벽과 충돌체크   4. 프로젝트이름 변경

--- resource.h
+++ resource.h
@@ -7,12 +7,18 @@
 #define IDB_BITMAP3                     105
 #define IDB_BITMAP4                     106
 #define IDB_BITMAP5                     108
+#define IDB_BITMAP6                     111
+#define IDB_BITMAP7                     112
+#define IDB_BITMAP8                     113
+#define IDB_BITMAP9                     115
+#define IDB_BITMAP10                    119
+#define IDB_BITMAP11                    120
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_RESOURCE_VALUE        121
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
