988ec3ed60160391806b2e6138ed41b17b0b5a4d
Seungwon <seungwon604@gmail.com>
2021-12-14 00:43:00 +0900

    원격환경 복구

--- Multi_game_client/Bomberman_project/Bomberman_project.cpp
+++ Multi_game_client/Bomberman_project/Bomberman_project.cpp
@@ -108,8 +108,8 @@ void PrintMap() {
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
 {
-	AllocConsole();
-	freopen("CONOUT$", "wt", stdout);
+	/*AllocConsole();
+	freopen("CONOUT$", "wt", stdout);*/
 	
 	//자동 리셋 이벤트 생성 (비신호 시작)
 	hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
@@ -177,7 +177,7 @@ DWORD WINAPI ClientMain(LPVOID arg)
 	if (retval == SOCKET_ERROR) err_quit("connect()");
 
 	//아이피, 포트번호 입력 대기
-	//WaitForSingleObject(hEvent, INFINITE);
+	WaitForSingleObject(hEvent, INFINITE);
 
 	char IP_NUM[16 + 3 + 1];
 	u_short PORT_NUM;
@@ -192,10 +192,10 @@ DWORD WINAPI ClientMain(LPVOID arg)
 	SOCKADDR_IN serveraddr;
 	ZeroMemory(&serveraddr, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
-	//serveraddr.sin_addr.s_addr = inet_addr(IP_NUM);
-	//serveraddr.sin_port = htons(PORT_NUM);
-	serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	serveraddr.sin_port = htons(10000);
+	serveraddr.sin_addr.s_addr = inet_addr(IP_NUM);
+	serveraddr.sin_port = htons(PORT_NUM);
+	/*serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serveraddr.sin_port = htons(10000);*/
 
 	retval = connect(sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
 	if (retval == SOCKET_ERROR) err_quit("connect()");
@@ -367,7 +367,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE:
 
 		//접속설정 대화상자 생성 (아이피, 포트번호 입력)
-		//DialogBox(g_hInst, MAKEINTRESOURCE(IDD_DIALOG2), hwnd, (DLGPROC)ConnectSettingDlgProc);
+		DialogBox(g_hInst, MAKEINTRESOURCE(IDD_DIALOG2), hwnd, (DLGPROC)ConnectSettingDlgProc);
 
 		//로그인 대화상자 생성
 		DialogBox(g_hInst, MAKEINTRESOURCE(IDD_DIALOG1), hwnd, (DLGPROC)LoginDlgProc);
