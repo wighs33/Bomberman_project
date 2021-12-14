1f9fd05729e82d327b6dfa7a0591d4c2560ffc68
yuntae-choi <jinyeong8229@gmail.com>
2021-12-14 15:03:47 +0900

    폭발 3이상 애니메이션 버그 수정
    클라이언트가 recv를 리턴할 시간을 줌

--- Multi_game_server/Multi_game_server/Object.cpp
+++ Multi_game_server/Multi_game_server/Object.cpp
@@ -28,6 +28,7 @@ static void SendDestroyedRock(array<Session, MAX_USER>& clients, int ix, int iy)
 			del_obj_packet.ix = ix;
 			del_obj_packet.iy = iy;
 			pl.Do_send(sizeof(del_obj_packet), &del_obj_packet);
+			this_thread::sleep_for(10ms);
 		}
 	}
 }
@@ -43,6 +44,7 @@ static void SendCreatedItem(array<Session, MAX_USER>& clients, int ix, int iy, i
 			create_iteam_packet.ix = ix;
 			create_iteam_packet.iy = iy;
 			pl.Do_send(sizeof(create_iteam_packet), &create_iteam_packet);
+			this_thread::sleep_for(10ms);
 		}
 	}
 }
@@ -59,6 +61,7 @@ static void SendExplosionStart(array<Session, MAX_USER>& clients, int ix, int iy
 			check_explosion_packet.iy = iy;
 			check_explosion_packet.isActive = true;
 			pl.Do_send(sizeof(check_explosion_packet), &check_explosion_packet);
+			this_thread::sleep_for(10ms); 
 		}
 	}
 }
@@ -81,12 +84,12 @@ void Bomb::Explode(tileArr<int, tile_max_w_num, tile_max_h_num>& objectMap, arra
 	explosionMapIndexs.emplace_back(bomb_ix, bomb_iy);
 	SendExplosionStart(clients, bomb_ix, bomb_iy);
 
-	Sleep(1);
+    //Sleep(1);
 
 	//폭탄 위 체크
 	for (int i = 1; i <= _power; ++i) {
-		if (_power == 3 && i == 2) Sleep(1);
-		if (_power >= 4 && (i == 2 || i == 4)) Sleep(1);
+		//if (_power == 3 && i == 2) Sleep(1);
+		//if (_power >= 4 && (i == 2 || i == 4)) Sleep(1);
 		//범위 체크
 		if (bomb_iy - i == -1) break;
 		//블럭 체크
@@ -114,12 +117,12 @@ void Bomb::Explode(tileArr<int, tile_max_w_num, tile_max_h_num>& objectMap, arra
 		SendExplosionStart(clients, bomb_ix, bomb_iy - i);
 	}
 
-	Sleep(1);
+	//Sleep(1);
 
 	//폭탄 아래 체크
 	for (int i = 1; i <= _power; ++i) {
-		if (_power == 3 && i == 2) Sleep(1);
-		if (_power >= 4 && (i == 2 || i == 4)) Sleep(1);
+		//if (_power == 3 && i == 2) Sleep(1);
+		//if (_power >= 4 && (i == 2 || i == 4)) Sleep(1);
 		//범위 체크
 		if (bomb_iy + i == tile_max_h_num) break;
 		//블럭 체크
@@ -148,12 +151,12 @@ void Bomb::Explode(tileArr<int, tile_max_w_num, tile_max_h_num>& objectMap, arra
 		SendExplosionStart(clients, bomb_ix, bomb_iy + i);
 	}
 
-	Sleep(1);
+	//Sleep(1);
 
 	//폭탄 왼쪽 체크
 	for (int i = 1; i <= _power; ++i) {
-		if (_power == 3 && i == 2) Sleep(1);
-		if (_power >= 4 && (i == 2 || i == 4)) Sleep(1);
+		//if (_power == 3 && i == 2) Sleep(1);
+		//if (_power >= 4 && (i == 2 || i == 4)) Sleep(1);
 		//범위 체크
 		if (bomb_ix - i == -1) break;
 		//블럭 체크
@@ -182,12 +185,12 @@ void Bomb::Explode(tileArr<int, tile_max_w_num, tile_max_h_num>& objectMap, arra
 		SendExplosionStart(clients, bomb_ix - i, bomb_iy);
 	}
 
-	Sleep(1);
+	//Sleep(1);
 
 	//폭탄 오른쪽 체크
 	for (int i = 1; i <= _power; ++i) {
-		if (_power == 3 && i == 2) Sleep(1);
-		if (_power >= 4 && (i == 2 || i == 4)) Sleep(1);
+		//if (_power == 3 && i == 2) Sleep(1);
+		//if (_power >= 4 && (i == 2 || i == 4)) Sleep(1);
 		//범위 체크
 		if (bomb_ix + i == tile_max_w_num) break;
 		//블럭 체크
